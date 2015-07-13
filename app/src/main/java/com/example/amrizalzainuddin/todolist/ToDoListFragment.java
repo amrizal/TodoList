1b995ca67dcb6f7ed0da16b89c1495ab8c59750a
Amrizal <amrizal@outlook.com>
2015-07-13 10:55:23 +0800

    Page 131

---
+++ app/src/main/java/com/example/amrizalzainuddin/todolist/ToDoListFragment.java
@@ -0,0 +1,9 @@
+package com.example.amrizalzainuddin.todolist;
+
+import android.app.ListFragment;
+
+/**
+ * Created by amrizal.zainuddin on 13/7/2015.
+ */
+public class ToDoListFragment extends ListFragment {
+}
--- app/src/main/java/com/example/amrizalzainuddin/todolist/TodoListActivity.java
+++ app/src/main/java/com/example/amrizalzainuddin/todolist/TodoListActivity.java
@@ -1,5 +1,8 @@
 package com.example.amrizalzainuddin.todolist;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -13,37 +16,28 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 
-public class TodoListActivity extends ActionBarActivity {
+public class TodoListActivity extends ActionBarActivity implements NewItemFragment.OnNewItemAddedListener {
+
+    private ArrayAdapter<String> aa;
+    private ArrayList<String> todoItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
 
-        ListView myListView = (ListView)findViewById(R.id.myListView);
-        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
+        FragmentManager fm = getFragmentManager();
+        ToDoListFragment toDoListFragment = (ToDoListFragment)fm.findFragmentById(R.id.ToDoListFragment);
 
-        final ArrayList<String> todoItems = new ArrayList<String>();
-
-        final ArrayAdapter<String> aa;
+        todoItems = new ArrayList<String>();
         aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
 
-        myListView.setAdapter(aa);
+        toDoListFragment.setListAdapter(aa);
+    }
 
-        myEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction() == KeyEvent.ACTION_DOWN)
-                    if((keyCode == KeyEvent.KEYCODE_DPAD_CENTER) ||
-                            (keyCode == KeyEvent.KEYCODE_ENTER)){
-                        todoItems.add(0, myEditText.getText().toString());
-                        aa.notifyDataSetChanged();
-                        myEditText.setText("");
-                        return true;
-                    }
-                return false;
-            }
-        });
+    public  void onNewItemAdded(String newItem){
+        todoItems.add(newItem);
+        aa.notifyDataSetChanged();
     }
 
     @Override
